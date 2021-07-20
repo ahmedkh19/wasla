964cce457063f281be92e408a92db5b3f8ce3759
fake555 <87621196+fake555@users.noreply.github.com>
2021-07-20 23:13:18 +0000

    Blogs Dashboard

--- resources/views/panels/scripts.blade.php
+++ resources/views/panels/scripts.blade.php
@@ -11,3 +11,30 @@
 {{-- page script --}}
 @yield('page-script')
 {{-- page script --}}
+
+{{-- CKEditor --}}
+<script src="https://cdn.ckeditor.com/ckeditor5/29.0.0/classic/ckeditor.js"></script>
+<script>
+var BlogDescription = document.getElementById('BlogDescription');
+if (BlogDescription) {
+    ClassicEditor
+        .create( document.querySelector( '#BlogDescription' ) )
+        .catch( error => {
+            console.error( error );
+        } );
+}
+</script>
+{{-- BlogImage --}}
+<script>
+var BlogImage = document.getElementById('BlogImage');
+if (BlogImage) {
+	BlogImage.addEventListener("change", function() {
+	    const [file] = BlogImage.files
+		if (file) {
+			BlogActuallImage.src = URL.createObjectURL(file)
+		}
+	    BlogActuallImage.classList = '';
+	    BlogSpan.innerHTML = '';
+	});
+}
+</script>
