c561d1c1ad219cbf8645ff75c4c35b05d0272ff1
fake555 <87621196+fake555@users.noreply.github.com>
2021-07-24 00:58:39 +0000

    update settings ==fake555==

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -6,6 +6,7 @@ use App\Models\Setting;
 use Hamcrest\Core\Set;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 
 class SettingController extends Controller
 {
@@ -17,15 +18,48 @@ class SettingController extends Controller
 
     public function update(Request $request)
     {
-       // return $request;
+    
+        $request->validate([
+            'slider_images.*' => 'mimes:jpg,jpeg',
+        ]);
+
         try {
             DB::beginTransaction();
-          //  $images = Setting::where('key', '=', 'slider_images')->first();
+          	$images = Setting::where('key', '=', 'slider_images')->first();
             $email = Setting::where('key', '=', 'email')->first();
             $whatsapp = Setting::where('key', '=', 'whatsapp')->first();
             $twitter = Setting::where('key', '=', 'twitter')->first();
 
-           // $images->update(['key' => 'slider_images', 'value' => $request->images]);
+		    $the_images = array();
+
+			// edit images
+			if (isset($request->keep_images)):
+				foreach($request->keep_images as $old_image):
+					if (in_array($old_image,json_decode($images->value))):
+						$the_images[] = $old_image;
+					endif;
+				endforeach;
+			endif;
+
+			// save images
+			if (isset($request->slider_images)):
+				foreach($request->slider_images as $image):
+					$image_name = uniqid() . '-' . $image->getClientOriginalName();
+					$image->move(public_path('images/settings/'), $image_name);
+				    $the_images[] = $image_name;
+				endforeach;
+			endif;
+			
+			// delete images
+			if (isset($images->value) && !empty(json_decode($images->value))):
+				foreach(json_decode($images->value) as $db_image):
+					if (!in_array($db_image,$the_images)):
+						File::delete(public_path('images/settings/') . $db_image);
+					endif;
+				endforeach;
+			endif;
+
+           	$images->update(['key' => 'slider_images', 'value' => json_encode($the_images, JSON_UNESCAPED_UNICODE)]);
             $email->update(['key' => 'email', 'value' => $request->email]);
             $whatsapp->update(['key' => 'whatsapp', 'value' => $request->whatsapp]);
             $twitter->update(['key' => 'twitter', 'value' => $request->twitter]);
